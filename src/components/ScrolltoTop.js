4523e783fd11fd963ee1397c03f8ae5528a6c7d0
DerekDardzinski <dardzinski.derek@gmail.com>
2020-12-27 12:49:23 -0500

    working on scrolling

--- src/components/ScrolltoTop.js
+++ src/components/ScrolltoTop.js
@@ -4,9 +4,12 @@ import { useLocation } from "react-router-dom";
 export default function ScrollToTop() {
   const { pathname } = useLocation();
   console.log(pathname);
+  // console.log(window.screenX);
+  // console.log(window.screenY);
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    // window.scrollTo(0, 0);
+    document.getElementsByClassName("home-main").ScrollTop = 0;
     console.log("Just Scrolled");
   }, [pathname]);
 
